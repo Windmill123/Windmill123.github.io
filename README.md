9f7b6bd65a1340de9e0e49ecd64fa14ce84e2eac
huwei123 <hw_hw111@163.com>
2019-03-08 12:41:15 +0800

    start

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Windmill123.github.io
+
+[-[]-]
+{-{}-} 数据
+(-()-) 内容
+<-<>-> 节点 
+config.title
---
+++ make/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*- 
+import re
+import json
+import copy
+import string
+# defaultencoding = 'utf-8'
+# if sys.getdefaultencoding() != defaultencoding:
+#     reload(sys)
+#     sys.setdefaultencoding(defaultencoding)
+print("123")
